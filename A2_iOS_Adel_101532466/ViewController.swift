b36d5acec9bc8dca02539550776d8b736799d0f9
adel alhajhussein <adel.alhajhussein@georgebrown.ca>
2026-04-09 11:15:44 -0400

    Initial Commit

---
+++ A2_iOS_Adel_101532466/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Adel_101532466
+//
+//  Created by Adel Alhajhussein on 2026-04-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
